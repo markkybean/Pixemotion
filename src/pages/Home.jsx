bf8b4c39fa4ab0492ebd08d22ba11358d15615d8
markkybean <marksantos320@gmail.com>
2024-02-06 02:41:58 +0800

    added picture upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,88 +10,146 @@ import {
   FormControl,
   FormLabel,
   Input,
-  Divider
+  Divider,
+  Grid,
 } from "@chakra-ui/react";
 import Pix from "./Pix";
 import firebaseApp from "./firebaseConfig";
-import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-import { getFirestore, addDoc, collection, Timestamp, onSnapshot } from 'firebase/firestore';
+import {
+  getAuth,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  addDoc,
+  collection,
+  Timestamp,
+  onSnapshot,
+} from "firebase/firestore";
+import { storage } from "./firebaseConfig";
+import { ref, uploadBytes, getDownloadURL, listAll } from "firebase/storage";
+
+
+import { v4 } from "uuid";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
-
   const auth = getAuth(firebaseApp);
   const db = getFirestore(firebaseApp);
 
   let navigate = useNavigate();
 
-  const [userProfile, setUserProfile] = useState('')
-  const [pix, setPix] = useState('');
+  const [userProfile, setUserProfile] = useState("");
+  const [pix, setPix] = useState("");
   const [pixs, setPixs] = useState([]);
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageList, setImageList] = useState([]);
+
+  const imageListRef = ref(storage, "images/");
 
   const [buttonLoading, setButtonLoading] = useState(false);
 
+  useEffect(() => {
 
-  
-  useEffect(()=>{
+    listAll(imageListRef)
+    .then((response) => {
+      const promises = response.items.map((item) => getDownloadURL(item));
+      return Promise.all(promises);
+    })
+    .then((urls) => {
+      setImageList(urls);
+    })
+    .catch((error) => {
+      console.error("Error listing images:", error);
+    });
 
-    //Authentication
-    onAuthStateChanged(auth, (user)=>{
-      if(user){
+
+    // Authentication
+    const authStateListener = onAuthStateChanged(auth, (user) => {
+      if (user) {
         setUserProfile({
           email: user.email,
-          name: user.displayName
-        })
-      }else{
-        navigate('/login');
+          name: user.displayName,
+        });
+      } else {
+        navigate("/login");
       }
     });
 
-    // retrieved pix
-
-    onSnapshot(collection(db, "pixs"), snapshot => {
-
-      setPixs(snapshot.docs.map(p=>p.data()));
-      // const pixList = [];
-      // snapshot.forEach(px=>{
-      //   pixList.push(px.data())
-      // })
-      // setPixs(pixList);
+    // Retrieve pix
+    const pixsListener = onSnapshot(collection(db, "pixs"), (snapshot) => {
+      setPixs(snapshot.docs.map((p) => p.data()));
     });
 
-  },[])
+    // Cleanup function
+    return () => {
+      authStateListener(); // Unsubscribe from auth state changes
+      pixsListener(); // Unsubscribe from pixs collection changes
+    };
+  }, []);
 
-  const createPix = () => { 
+  const createPix = async () => {
     setButtonLoading(true);
-    if(pix!==''){
 
-      const pixData = {
-        body: pix,
-        user_email: userProfile.email,
-        name: userProfile.name,
-        date_posted: Timestamp.now()
+    try {
+      if (pix !== "") {
+        const pixData = {
+          body: pix,
+          user_email: userProfile.email,
+          name: userProfile.name,
+          date_posted: Timestamp.now(),
+        };
+
+        // Add Pix data to Firestore
+        const docRef = await addDoc(collection(db, "pixs"), pixData);
+
+        // Upload image to Firebase Storage
+        uploadImage(docRef.id);
+
+        setPix("");
+      } else {
+        alert("Pix cannot be empty");
       }
-      addDoc( collection(db, "pixs"), pixData).then(()=>{
-        setPix('');
-        setButtonLoading(false);
-      });
-
-    }else{
-      alert("Pix cannot be empty").then(()=>{
-        setButtonLoading(false);
-      });
+    } catch (error) {
+      console.error("Error creating Pix:", error);
+    } finally {
+      setButtonLoading(false);
     }
-  }
+  };
 
-  const logout = () =>{
-    signOut(auth).then(() =>{
+  const uploadImage = async (pixId) => {
+    if (imageUpload === null) return null;
+  
+    const imageRef = ref(
+      storage,
+      `images/${pixId}_${imageUpload.name + v4()}`
+      // getDownloadURL(snapshot.ref).then((url)=>{
+      //   setImageList((prev) => [...prev, url])
+      // })
+      
+    );
+  
+    try {
+      await uploadBytes(imageRef, imageUpload);
+      const downloadURL = await getDownloadURL(imageRef);
+      return downloadURL;
+    } catch (error) {
+      console.error("Error uploading image:", error);
+      return null;
+    }
+  };
+  
+  const logout = () => {
+    signOut(auth).then(() => {
       navigate("/login");
     });
-  }
+  };
 
   return (
     <Container maxW="1024" pt="100">
+      
       <Heading>Pixemotion</Heading>
       <Text>Every Image Tells a Story</Text>
       <Flex>
@@ -104,36 +162,62 @@ function Home() {
             </Button>
           </Card>
         </Box>
+        {/* {imageList.map((url)=>{
+        return <img src={url} />
+      })} */}
 
-        <Spacer/>
+        <Spacer />
         <Box w="700px">
-        <Card mt={5} p={5}>
+          <Card mt={5} p={5}>
             <FormControl>
-                <FormLabel>What's up?</FormLabel>
-                <Input disabled={buttonLoading} type="text" onChange={(e)=>{setPix(e.target.value)}} value={pix} />
+              <FormLabel>What's up?</FormLabel>
+              <Grid templateColumns="3fr 1fr" gap={4}>
+                <Input
+                  w="100%"
+                  disabled={buttonLoading}
+                  type="text"
+                  onChange={(e) => setPix(e.target.value)}
+                  value={pix}
+                />
+                <Input
+                  w="100%"
+                  type="file"
+                  accept="image/*"
+                  onChange={(e) => {
+                    setImageUpload(e.target.files[0]);
+                  }}
+                />
+              </Grid>
             </FormControl>
-            <Button isLoading={buttonLoading} size="sm" w="100px" mt={3} onClick={createPix}>Pix</Button>
+            <Button
+              isLoading={buttonLoading}
+              size="sm"
+              w="100px"
+              mt={3}
+              onClick={createPix}
+            >
+              Pix
+            </Button>
           </Card>
 
           <Divider m={5}></Divider>
 
-          {
-            pixs.map((pixRecord) => (
-              <Pix
-                key={pixRecord.id}
-                body={pixRecord.body}
-                email={pixRecord.user_email}
-                name={pixRecord.name}
-                date_posted={pixRecord.date_posted.toDate().toString()}
-              ></Pix>
-            ))
-          }
-
-
-          
+          {pixs.map((pixRecord, index) => (
+            <Pix
+              key={pixRecord.id}
+              body={pixRecord.body}
+              email={pixRecord.user_email}
+              name={pixRecord.name}
+              date_posted={pixRecord.date_posted.toDate().toString()}
+              imageUrl={pixRecord.imageUrl}
+              images={imageList} // Pass the imageList as the images prop
+              imageIndex={index} // Pass the index as the imageIndex prop
+            ></Pix>
+          ))}
         </Box>
       </Flex>
     </Container>
   );
 }
+
 export default Home;
--- src/pages/Pix.jsx
+++ src/pages/Pix.jsx
@@ -1,26 +1,82 @@
-import {
-    Container,
-    Box,
-    Flex,
-    Heading,
-    Card,
-    Text,
-    Divider,
-    Button,
-    Spacer,
-    FormControl,
-    FormLabel,
-    Input
-  } from "@chakra-ui/react";
+// Pix.js
+import { Card, CardBody, CardFooter, CardHeader, Flex, Box, Text, Image, Button, IconButton, Avatar, Heading } from "@chakra-ui/react";
+import { BiLike, BiChat, BiShare } from 'react-icons/bi';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import React from "react";
 
-function Pix({body, email, name, date_posted}){
-    return(
-        <Card mt={5} pb={5} px={5} pt={2}>
-            <Text fontWeight="bold"> {name} </Text>
-            <Text fontSize="xs" color="gray">  {date_posted} </Text>
-            <Divider my={2} color="lightgray"></Divider>
-            <Text> {body} </Text>
+const Pix = ({ body, email, name, date_posted, imageUrl, images, imageIndex }) => {
+  return (
+    <div>
+
+      {/* <Card> */}
+        {/* <CardHeader>
+          <Text fontWeight={"bold"} >{name}</Text>
+          <Text fontSize={"small"} >{date_posted}</Text>
+        </CardHeader>
+        <CardBody>
+          <Text>{body}</Text>
+          {/* <p>{email}</p> */}
+          {/* <Card>
+            {images.length > 0 && (
+              <img src={images[imageIndex % images.length]} alt={`Image ${imageIndex}`} />
+            )}
           </Card>
-    )
-}
+        </CardBody> */}
+      {/* </Card> */}
+
+
+      <Card>
+        <CardHeader>
+          <Flex spacing='4'>
+            <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+              <Avatar name='Segun Adebayo' src='https://uxwing.com/wp-content/themes/uxwing/download/peoples-avatars/avatar-icon.png' />
+
+              <Box>
+                <Heading size='sm'>{name}</Heading>
+                <Text fontSize="small">{date_posted}</Text>
+              </Box>
+            </Flex>
+            <IconButton
+              variant='ghost'
+              colorScheme='gray'
+              aria-label='See menu'
+              icon={<BsThreeDotsVertical />}
+            />
+          </Flex>
+        </CardHeader>
+        <CardBody>
+          <Text>
+          {body}
+          </Text>
+        </CardBody>
+        <Box p={"20px"}>
+        {images.length > 0 && (
+              <img src={images[imageIndex % images.length]} alt={`Image ${imageIndex}`} />
+            )}
+        </Box>
+
+        <CardFooter
+          justify='space-between'
+          flexWrap='wrap'
+          sx={{
+            '& > button': {
+              minW: '136px',
+            },
+          }}
+        >
+          <Button flex='1' variant='ghost' leftIcon={<BiLike />}>
+            Like
+          </Button>
+          <Button flex='1' variant='ghost' leftIcon={<BiChat />}>
+            Comment
+          </Button>
+          <Button flex='1' variant='ghost' leftIcon={<BiShare />}>
+            Share
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+};
+
 export default Pix;
