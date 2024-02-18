1cd3b592e0695598dba23659a8dcc79c1265e323
markkybean <marksantos320@gmail.com>
2024-02-18 20:28:43 +0800

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/auth/Login';
-import Register from './pages/auth/Register';
-import Home from './pages/Home';
-import Layout from './pages/Layout';
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
 
 function App() {
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout/>}>
-      <Route index element={<Home/>} />
-      <Route path="login" element={<Login/>}></Route>
-      <Route path="register" element={<Register/>}></Route>
-    
-        
-
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-    
   );
 }
 
-export default App
+export default App;
